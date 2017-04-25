51402628ad0c0930feb622d2c46e66c8823b0365
NaydenMominski <mominski@abv.bg>
2017-04-25 13:45:51 +0300

    show and hide function

--- js/controllers/userController.js
+++ js/controllers/userController.js
@@ -7,7 +7,7 @@ import notifier from 'js/notifier.js'
 
 
 export default {
-    login: function(username, password) {
+    login: function(context) {
         templates.load('login')
             .then(function(templateHtml) {
                 $('#main').html(templateHtml);
@@ -21,23 +21,30 @@ export default {
                 .then(function() {
                     return data.users.login(username, password)
                 })
-                .then(function(data) {
+                .then(function(response) {
                     $('#formLogin input[name=login-username]').val('');
                     $('#formLogin input[name=login-passwd]').val('');
 
                     $('#btn-navlogout').show();
                     $('#btn-navlogin').hide();
-                    $('.top-cart-contain').show();
+                    $('.cart_cur_block').show();
 
                     toastr.success('User Log In!', 'Welcome, ' + sessionStorage.getItem('username') + '!', { timeOut: 2000 });
-                })
-                .catch(function(err) {
-                    toast.error("ERROR");
+
+                    $('#welcome-msg').show().text(`Welcome, ${sessionStorage.getItem('username')} !`);
+                    context.redirect('#/home');
+                }, function(error) {
+                    toastr.error('LogIn unsuccessful!');
+                    context.redirect('#/home');
                 });
+
+            // .catch(function(err) {
+            //     toastr.error("ERROR");
+            // });
 
         });
     },
-    register: function(username, password) {
+    register: function(context) {
         templates.load('register')
             .then(function(templateHtml) {
                 $('#main').html(templateHtml);
@@ -62,15 +69,17 @@ export default {
         });
 
     },
-    logout: function() {
+    logout: function(context) {
         $('#btn-navlogout').click(function() {
             data.users.logout()
                 .then(function() {
                     toastr.success(sessionStorage.getItem('username') + ' Log Out');
                     sessionStorage.clear();
+
                     $('#btn-navlogout').hide();
                     $('#btn-navlogin').show();
-                    $('.top-cart-contain').hide();
+                    $('.cart_cur_block').hide();
+                    context.redirect('#/home');
                 })
                 .catch(function() {
                     toastr.error('No User to Log Out ');
